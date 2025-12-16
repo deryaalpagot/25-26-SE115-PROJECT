062a4ea6456f5aa2e964860487b77f7e87b5a358
deryaalpagot <deryaalpagot@gmail.com>
2025-12-16 12:47:21 +0300

    Add files via upload

---
+++ main/src/Main.java
@@ -0,0 +1,44 @@
+// Main.java — Students version
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static final int MONTHS = 12;
+    static final int DAYS = 28;
+    static final int COMMS = 5;
+    static String[] commodities = {"Gold", "Oil", "Silver", "Wheat", "Copper"};
+    static String[] months = {"January","February","March","April","May","June",
+            "July","August","September","October","November","December"};
+    public static int[][][] profitData;
+
+
+    // ======== REQUIRED METHOD LOAD DATA (Students fill this) ========
+    public static void loadData() {
+    }
+
+    // ======== 10 REQUIRED METHODS (Students fill these) ========
+
+    public static String mostProfitableCommodityInMonth(int month) {
+
+    }
+
+    public static int totalProfitOnDay(int month, int day) {
+        return 1234;
+    }
+
+    public static int commodityProfitInRange(String commodity, int from, int to) {
+        return 1234;
+    }
+
+    public static int bestDayOfMonth(int month) {
+        return 1234;
+    }
+
+    public static String bestMonthForCommodity(String comm) {
+        return "DUMMY";
+    }
+
+    public static int consecutiveLossDays(String comm) {
+        return 1234;
+    }
+    }
