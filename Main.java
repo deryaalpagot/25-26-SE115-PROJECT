c63d5a3dbeb1bdfccfa20b10b872581cadb8235a
deryaalpagot <deryaalpagot@gmail.com>
2025-12-20 17:34:55 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -178,11 +178,46 @@ public class Main {
     }
 
     public static String compareTwoCommodities(String c1, String c2) {
-        return "DUMMY is better by 1234";
+        int i1 = -1, i2 = -1;
+
+        for (int i = 0; i < COMMS; i++) {
+            if (commodities[i].equals(c1)) i1 = i;
+            if (commodities[i].equals(c2)) i2 = i;
+        }
+        if (i1 == -1 || i2 == -1) return "INVALID_COMMODITY";
+
+        int sum1 = 0, sum2 = 0;
+        for (int m = 0; m < MONTHS; m++) {
+            for (int d = 0; d < DAYS; d++) {
+                sum1 += profit[m][d][i1];
+                sum2 += profit[m][d][i2];
+            }
+        }
+
+        if (sum1 > sum2) return c1 + " is better by " + (sum1 - sum2);
+        if (sum2 > sum1) return c2 + " is better by " + (sum2 - sum1);
+        return "Equal";
     }
 
     public static String bestWeekOfMonth(int month) {
-        return "DUMMY";
+        if (month < 0 || month >= MONTHS) return "INVALID_MONTH";
+
+        int bestWeek = 1;
+        int max = Integer.MIN_VALUE;
+
+        for (int w = 0; w < 4; w++) {
+            int sum = 0;
+            for (int d = w * 7; d < (w + 1) * 7; d++) {
+                for (int c = 0; c < COMMS; c++) {
+                    sum += profit[month][d][c];
+                }
+            }
+            if (sum > max) {
+                max = sum;
+                bestWeek = w + 1;
+            }
+        }
+        return "Week " + bestWeek;
     }
 
     public static void main(String[] args) {
